b2c68fdf8390e966e8a1882df8fe2bea1f7fb337
Marcelino <marcelino.malmeidan.pensa@gmail.com>
2020-05-05 11:18:13 -0500

    fix for getting parameters from launch file

--- src/odom_relay_twist_body.cpp
+++ src/odom_relay_twist_body.cpp
@@ -5,7 +5,7 @@
 #include "nav_msgs/Odometry.h"
 #include <tf/tf.h>
 
-ros::Publisher odom_pub;
+ros::Publisher odom_pub_;
 
 geometry_msgs::Vector3 set_vector3(const double &x, const double &y, const double &z) {
   geometry_msgs::Vector3 v;
@@ -33,20 +33,23 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
   odom.header.frame_id = "vislam_odom";
   odom.child_frame_id = "odom_frame";
   odom.twist.twist.linear = convert_to_body_frame(quat, vel);
-  odom_pub.publish(odom);
+  odom_pub_.publish(odom);
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "odom_relay_tf");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "~");
+  ros::NodeHandle n("~");
 
   std::string odom_topic, output_topic;
   n.getParam("odom_topic", odom_topic);
   n.getParam("output_topic", output_topic);
 
   ros::Subscriber sub_odom = n.subscribe(odom_topic, 5, odomCallback);
-  odom_pub = n.advertise<nav_msgs::Odometry>(output_topic, 5);
+  odom_pub_ = n.advertise<nav_msgs::Odometry>(output_topic, 5);
   
+  ROS_INFO("[odom_relay]: Subscribed to topic: %s", sub_odom.getTopic().c_str());
+  ROS_INFO("[odom_relay]: Publishing into topic: %s", odom_pub_.getTopic().c_str());
+
   ros::spin();
 
   return 0;
